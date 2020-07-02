d33f066836259df92b898f307904a6a094e59dfe
Stuart Stone <stuartstone123@gmail.com>
2020-07-02 14:46:22 +0100

    migrate gradle to kotlin script dsl, use stub-lite for android

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,90 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.ofSourceSet
+import com.google.protobuf.gradle.plugins
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
+val grpcVersion = "1.30.0"
+val grpcKotlinVersion = "0.1.3"
+val protobufVersion = "3.12.2"
+val coroutinesVersion = "1.3.7"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.72"
+    id("com.google.protobuf") version "0.8.12"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("javax.annotation:javax.annotation-api:1.2")
+    implementation("com.google.protobuf:protobuf-java-util:$protobufVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    runtimeOnly("io.grpc:grpc-netty-shaded:$grpcVersion")
+}
+
+group = "com.beerfie.grpc"
+version = "0.0.1"
+
+sourceSets {
+    main {
+        proto.srcDir("../proto/alarmgrpc")
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc") {
+                    option("lite")
+                }
+                id("grpckt") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/stonest/alarm-protobuf-gen")
+            credentials {
+                username = System.getenv("GH_USERNAME")
+                password = System.getenv("GH_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("showeralarm") {
+            from(components["java"])
+        }
+    }
+}
